f359788c31ad61622853ff990d05fe2522ab4df6
Muriel Pacheco <murieldps@gmail.com>
2023-01-02 10:57:09 -0300

    feat: styling Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
+import { Timer, Scroll } from 'phosphor-react';
+import {NavLink} from 'react-router-dom';
+import { HeaderContainer } from './styles';
+
 export function Header() {
 	return (
-		<h1>Header</h1>
+		<HeaderContainer>
+			<strong>Pomodoro App</strong>
+			<nav>
+				<NavLink to="/" title='Timer'><Timer size={24}/></NavLink>
+				<NavLink to="/history" title='Histórico'><Scroll size={24} /></NavLink>
+			</nav>
+		</HeaderContainer>
 	);
 }
